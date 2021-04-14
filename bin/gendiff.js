02a668dd4297088e8fed9d3d4571fd702593cefc
vladbeer <vladbeer@bk.ru>
2021-04-15 00:58:28 +0300

    json

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,14 @@
 #!/usr/bin/env node
 import program from 'commander';
+import getDiff from '../src/engenie.js';
 
 program
   .version('0.0.1')
   .option('-f, --format <type>', 'output format', 'stylish')
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<filePath1> <filePath2>')
+  .action((filePath1, filePath2, cmdObj) => {
+    const diff = getDiff(filePath1, filePath2, cmdObj.format);
+    console.log(diff);
+  })
   .parse();
---
+++ src/engenie.js
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import _ from 'lodash';
+import path from 'path';
+
+export default (path1, path2, formatName = 'stylish') => {
+    const filePath1 = path.resolve('src', path1);
+    const fileObj1 = JSON.parse(fs.readFileSync(filePath1, 'utf-8'));
+    const filePath2 = path.resolve('src', path2);
+    const fileObj2 = JSON.parse(fs.readFileSync(filePath2, 'utf-8'));
+    const keys = _.sortBy(_.union(Object.keys(fileObj1), Object.keys(fileObj2)));
+    let result = '{\n';
+
+    for (let key of keys) {
+        if (fileObj1[key] === fileObj2[key]) {
+            result += `    ${key}: ${fileObj2[key]}\n`;
+        } else if (!_.has(fileObj2, key)) {
+            result += `  - ${key}: ${fileObj1[key]}\n`;
+        } else if (!_.has(fileObj1, key)) {
+            result += `  + ${key}: ${fileObj2[key]}\n`;
+        } else if (fileObj1[key] !== fileObj2[key]) {
+            result += `  - ${key}: ${fileObj1[key]}\n`;
+            result += `  + ${key}: ${fileObj2[key]}\n`;
+        }
+    }
+
+    return result += '}';
+
+}
